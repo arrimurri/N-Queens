61cd3cb6cb41d6681f156ca2829070c843825e75
Patrik <patrik.marjanen@helsinki.fi>
2011-02-18 18:23:46 +0200

    Some initial code. Just fleshing out things, loads of changes to come.

--- src/rio/BoardState.java
+++ src/rio/BoardState.java
@@ -1,5 +1,28 @@
 package rio;
 
 public class BoardState {
+	
+	private boolean[][] board;
+	
+	public BoardState(boolean[][] b) {
+		for(int i = 0; i < b.length; i++) {
+			for(int j = 0; j < b[i].length; i++) {
+				this.board[i][j] = b[i][j];
+			}
+		}
+	}
+	
+	public BoardState(int n) {
+		this.board = new boolean[n][n];
+		for(int i = 0; i < this.board.length; i++) {
+			for(int j = 0; j < this.board[i].length; i++) {
+				this.board[i][j] = false;
+			}
+		}
+	}
+	
+	public boolean isOccupied(int x, int y) {
+		return this.board[x][y];
+	}
 
 }
